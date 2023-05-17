f371af36360d305fd2addc100bca6a31ee9a2026
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-05-17 15:40:53 -0600

    Write unit test for gameboard

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -28,3 +28,88 @@ test("isSunk method returns true when ship is sunk", () => {
   ship.hit();
   expect(ship.isSunk()).toBe(true);
 });
+
+test("Gameboard factory returns object", () => {
+  expect(battleship.Gameboard()).toEqual(expect.any(Object));
+});
+
+test("Gameboard factory returns object with placeShip method", () => {
+  expect(battleship.Gameboard()).toHaveProperty("placeShip");
+});
+
+test("Gameboard factory returns object with receiveAttack method", () => {
+  expect(battleship.Gameboard()).toHaveProperty("receiveAttack");
+});
+
+test("Gameboard factory returns object with allSunk method", () => {
+  expect(battleship.Gameboard()).toHaveProperty("allSunk");
+});
+
+test("placeShip method places ship on board horizontally", () => {
+  const board = battleship.Gameboard();
+  const ship = battleship.Ship(3);
+  board.placeShip(ship, 0, 0, "horizontal");
+  expect(board.board[0][0].shipSpace).toEqual(true);
+  expect(board.board[0][1].shipSpace).toEqual(true);
+  expect(board.board[0][2].shipSpace).toEqual(true);
+  expect(board.board[0][0].shipPointer).toEqual(ship);
+  expect(board.board[0][1].shipPointer).toEqual(ship);
+  expect(board.board[0][2].shipPointer).toEqual(ship);
+});
+
+test("placeShip method places ship on board vertically", () => {
+  const board = battleship.Gameboard();
+  const ship = battleship.Ship(3);
+  board.placeShip(ship, 0, 0, "vertical");
+  expect(board.board[0][0].shipSpace).toEqual(true);
+  expect(board.board[1][0].shipSpace).toEqual(true);
+  expect(board.board[2][0].shipSpace).toEqual(true);
+  expect(board.board[0][0].shipPointer).toEqual(ship);
+  expect(board.board[1][0].shipPointer).toEqual(ship);
+  expect(board.board[2][0].shipPointer).toEqual(ship);
+});
+
+test("receiveAttack method records missed attack", () => {
+  const board = battleship.Gameboard();
+  board.receiveAttack(0, 0);
+  expect(board.board[0][0].shotAt).toBe(true);
+});
+
+test("receiveAttack method records hit attack", () => {
+  const board = battleship.Gameboard();
+  const ship = battleship.Ship(3);
+  board.placeShip(ship, 0, 0, "horizontal");
+  board.receiveAttack(0, 0);
+  expect(board.board[0][0].shotAt).toBe(true);
+  expect(board.board[0][0].shipSpace).toBe(true);
+  expect(ship.isSunk()).toBe(false);
+});
+
+test("receiveAttack method records sunk attack", () => {
+  const board = battleship.Gameboard();
+  const ship = battleship.Ship(3);
+  board.placeShip(ship, 0, 0, "horizontal");
+  board.receiveAttack(0, 0);
+  board.receiveAttack(0, 1);
+  board.receiveAttack(0, 2);
+  expect(ship.isSunk()).toBe(true);
+});
+
+test("allSunk method returns false when not all ships are sunk", () => {
+  const board = battleship.Gameboard();
+  const ship = battleship.Ship(3);
+  board.placeShip(ship, 0, 0, "horizontal");
+  board.receiveAttack(0, 0);
+  board.receiveAttack(0, 1);
+  expect(board.allSunk()).toBe(false);
+});
+
+test("allSunk method returns true when all ships are sunk", () => {
+  const board = battleship.Gameboard();
+  const ship = battleship.Ship(3);
+  board.placeShip(ship, 0, 0, "horizontal");
+  board.receiveAttack(0, 0);
+  board.receiveAttack(0, 1);
+  board.receiveAttack(0, 2);
+  expect(board.allSunk()).toBe(true);
+});
