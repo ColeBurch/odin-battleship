e7594f4f2b45cbc153104b7550af0f318b1d7ceb
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-05-17 15:08:02 -0600

    Wrote unit tests and Ship factory function

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,11 +1,30 @@
 const battleship = require("./index.js");
-const sum = battleship.sum;
-const subtract = battleship.subtract;
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("Ship factory returns object", () => {
+  expect(battleship.Ship(3)).toEqual(expect.any(Object));
 });
 
-test("subtract 1 - 2 to equal -1", () => {
-  expect(subtract(1, 2)).toBe(-1);
+test("Ship factory returns object with length property", () => {
+  expect(battleship.Ship(3)).toHaveProperty("length", 3);
+});
+
+test("Ship factory returns object with hit method", () => {
+  expect(battleship.Ship(3)).toHaveProperty("hit");
+});
+
+test("Ship factory returns object with isSunk method", () => {
+  expect(battleship.Ship(3)).toHaveProperty("isSunk");
+});
+
+test("isSunk method returns false when ship is not sunk", () => {
+  const ship = battleship.Ship(3);
+  expect(ship.isSunk()).toBe(false);
+});
+
+test("isSunk method returns true when ship is sunk", () => {
+  const ship = battleship.Ship(3);
+  ship.hit();
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
 });
--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
 import "./style.css";
 
-function component() {
-  const element = document.createElement("div");
-  element.classList.add("hello");
-
-  element.innerHTML = "Hello World";
-
-  sum(1, 2);
-
-  return element;
-}
-
-function sum(a, b) {
-  return a + b;
-}
-
-function subtract(a, b) {
-  return a - b;
-}
-
-document.body.appendChild(component());
+const Ship = (len) => {
+  const length = len;
+  let hits = 0;
+  const hit = () => {
+    hits++;
+  };
+  const isSunk = () => {
+    return hits === length;
+  };
+  return { length, hit, isSunk };
+};
 
 module.exports = {};
-module.exports.sum = sum;
-module.exports.subtract = subtract;
+module.exports.Ship = Ship;
